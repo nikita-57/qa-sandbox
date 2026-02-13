c4cefe8ea80e5d09cabd52d6044b5dd72c4e2af8
nikita-57 <nikitaprimi@gmail.com>
2026-02-13 12:11:50 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    allowedHosts: ["stage.qalab.pantatech.ru"],
-  },
-  preview: {
-    host: true,
-    allowedHosts: ["stage.qalab.pantatech.ru"],
-  },
-});
+    allowedHosts: true
+  }
+
+})
