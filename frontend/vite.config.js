edf1f29ad7db1fff0a4885fe77be5c11932090c0
nikita-57 <nikitaprimi@gmail.com>
2026-02-13 11:56:11 +0300

    change the Vite configuration to allow hosting on "stage.qalab.pantatech.ru" for both development and preview servers.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+    allowedHosts: ["stage.qalab.pantatech.ru"],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ["stage.qalab.pantatech.ru"],
+  },
+});
